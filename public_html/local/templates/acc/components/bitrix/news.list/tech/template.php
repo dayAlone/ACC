8213d6f5c3bd61a6d338dfe4fc409c21cb689a51
dayAlone <andrey.slider@gmail.com>
2014-10-20 21:26:14 +0600

    CSS

--- public_html/local/templates/acc/components/bitrix/news.list/tech/template.php
+++ public_html/local/templates/acc/components/bitrix/news.list/tech/template.php
@@ -12,15 +12,18 @@
             </a>
           </div>
           <?endif;?>
-          <div class="col-xs-7 col-sm-12">
+
+          <div class="col-xs-7 col-sm-12 tech__icons-frame">
             <a data-toggle="modal" data-target="#techDetail" href="#techDetail" data-url="<?=$item['DETAIL_PAGE_URL']?>" class="tech__title"><?=$item['NAME']?></a>
-            <div class="row">
-              <?if(strlen($item['VIDEO'])>0):?>
-                <div class="col-xs-6 col-sm-5"><a data-toggle="modal" data-target="#techMedia" href="#techMedia" class="tech__link tech__video"  data-url="<?=$item['DETAIL_PAGE_URL']?>?a=video"><?=svg('video')?>Видео</a></div>
-              <?endif;?>
-              <?if(count($item['IMAGES'])>0):?>
-                <div class="col-xs-6 col-sm-7"><a data-toggle="modal" data-target="#techMedia" href="#techMedia" class="tech__link tech__photos"  data-url="<?=$item['DETAIL_PAGE_URL']?>?a=photo"><?=svg('photos')?>Фотогалерея</a></div>
-              <?endif;?>
+            <div class="tech__icons">
+              <div class="row">
+                <?if(strlen($item['VIDEO'])>0):?>
+                  <div class="col-xs-6 col-sm-5"><a data-toggle="modal" data-target="#techMedia" href="#techMedia" class="tech__link tech__video"  data-url="<?=$item['DETAIL_PAGE_URL']?>?a=video"><?=svg('video')?>Видео</a></div>
+                <?endif;?>
+                <?if(count($item['IMAGES'])>0):?>
+                  <div class="col-xs-6 col-sm-7"><a data-toggle="modal" data-target="#techMedia" href="#techMedia" class="tech__link tech__photos"  data-url="<?=$item['DETAIL_PAGE_URL']?>?a=photo"><?=svg('photos')?>Фотогалерея</a></div>
+                <?endif;?>
+              </div>
             </div>
           </div>
         </div>
