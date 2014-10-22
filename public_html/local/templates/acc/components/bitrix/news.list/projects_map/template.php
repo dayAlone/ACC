b153c92460333990e835e3ae863b47712102f40c
dayAlone <andrey.slider@gmail.com>
2014-10-22 21:53:42 +0600

    Map

--- public_html/local/templates/acc/components/bitrix/news.list/projects_map/template.php
+++ public_html/local/templates/acc/components/bitrix/news.list/projects_map/template.php
@@ -132,7 +132,7 @@
 			var circle, cords, mapElement, mapOptions, line, path, objects = {'finished': {'lines' : [], 'markers':[]}, 'current': {'lines':[], 'markers':[]}};
 		    mapOptions = {
 		      zoom: 3,
-		      mapTypeId: google.maps.MapTypeId.SATELLITE,
+		      mapTypeId: google.maps.MapTypeId.HYBRID,
 		      //draggable: false,
 		      //zoomControl: false,
 		      scrollwheel: false,
@@ -146,7 +146,7 @@
 		        position: google.maps.ControlPosition.LEFT_CENTER
     		  },
 		      center: new google.maps.LatLng(66.69261210265907, 95.40570330969672),
-		      styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":0}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]}]
+		      styles: [{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000"},{"lightness":170},{"weight":0.1}]}]
 		    };
 		    mapElement = document.getElementById('bg_map');
 		    map = new google.maps.Map(mapElement, mapOptions);
