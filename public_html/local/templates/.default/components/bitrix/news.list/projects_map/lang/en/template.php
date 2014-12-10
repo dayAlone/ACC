3a0e625b36ac7133e0a452968720bda0139c4789
dayAlone <andrey.slider@gmail.com>
2014-12-10 21:00:13 +0500

    Projects

--- public_html/local/templates/.default/components/bitrix/news.list/projects_map/lang/en/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/projects_map/lang/en/template.php
@@ -1,8 +1,8 @@
 <?
-$MESS['ALL']      = "Все проекты";
-$MESS['CURRENT']  = "текущие<span>проекты</span>";
-$MESS['FINISHED'] = "ЗАВЕРШЕННЫЕ<span>проекты</span>";
-$MESS['SHOW']     = "Показывать";
-$MESS['LIST']     = "Список";
-$MESS['MAP']      = "Карту";
+$MESS['ALL']      = "All projects";
+$MESS['CURRENT']  = "Current<span>projects</span>";
+$MESS['FINISHED'] = "Completed<span>projects</span>";
+$MESS['SHOW']     = "Show";
+$MESS['LIST']     = "List";
+$MESS['MAP']      = "Map";
 ?>
--- public_html/local/templates/.default/components/bitrix/news.list/projects_map/lang/ru/template.php
+++
@@ -1,8 +0,0 @@
-<?
-$MESS['ALL']      = "All projects";
-$MESS['CURRENT']  = "Current <span>projects</span>";
-$MESS['FINISHED'] = "Completed <span>projects</span>";
-$MESS['SHOW']     = "Show";
-$MESS['LIST']     = "List";
-$MESS['MAP']      = "Map";
-?>
