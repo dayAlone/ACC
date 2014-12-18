328901804c698e69d569aa915674420c1b56e8d8
dayAlone <andrey.slider@gmail.com>
2014-12-18 21:13:02 +0500

    Changes

--- public_html/eng/news/index.php
+++ public_html/eng/news/index.php
@@ -25,18 +25,60 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/include/news_list.php');
 </div>
 <div class="page__divider m-margin-top"></div>
 <?
-$APPLICATION->IncludeComponent("bitrix:news.list", "news", 
-array(
-		"IBLOCK_ID"      => 6,
-		"NEWS_COUNT"     => "10",
-		"PARENT_SECTION" => $_GLOBALS['currentNewsSection'],
-		"SORT_BY1"       => "ACTIVE_FROM",
-		"SORT_ORDER1"    => "DESC",
-		"DETAIL_URL"     => "/news/#ELEMENT_CODE#/",
-		"CACHE_TYPE"     => "A",
-		"SET_TITLE"      => "N"
-   ),
-   false
+$APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"news", 
+	array(
+		"IBLOCK_ID" => "6",
+		"NEWS_COUNT" => "10",
+		"PARENT_SECTION" => $_GLOBALS["currentNewsSection"],
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"DETAIL_URL" => "/news/#ELEMENT_CODE#/",
+		"CACHE_TYPE" => "A",
+		"SET_TITLE" => "N",
+		"IBLOCK_TYPE" => "content_eng",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"CHECK_DATES" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_STATUS_404" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"PARENT_SECTION_CODE" => "",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"PAGER_TEMPLATE" => ".default",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Новости",
+		"PAGER_SHOW_ALWAYS" => "Y",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "Y",
+		"AJAX_OPTION_ADDITIONAL" => ""
+	),
+	false
 );
 if(strlen($_REQUEST['ELEMENT_CODE'])>0 && !isset($_GLOBALS['currentNewsSection']))
 {?>
